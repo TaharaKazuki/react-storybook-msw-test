23fb3e9ce6d7333d30558fa78d65851fae28ea8f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-03-06 09:17:14 +0900

    Add testing libraries and configuration for Form component

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,5 +1,24 @@
-import { expect, test } from 'vitest';
+import { composeStory } from '@storybook/react';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import { describe, expect, test, vi } from 'vitest';
 
-test('renders form', () => {
-  expect(true).toBe(true);
+import Meta, * as Stories from './Form.stories';
+
+describe('Form', () => {
+  test.concurrent('should submit correct data', async () => {
+    // Arrange
+    const submitHandler = vi.fn();
+
+    const Form = composeStory(Stories.Default, Meta);
+    render(<Form onSubmit={submitHandler} />);
+
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+
+    //Act
+    await userEvent.click(submitButton);
+
+    //Assert
+    expect(submitHandler).toHaveBeenCalledWith({ option: 'option-3' });
+  });
 });
